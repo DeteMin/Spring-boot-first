fab8b77b2d1e2045ffe0c1ec368999a2312ea921
DeteMin <516346543@qq.com>
2018-10-09 10:42:59 +0800

    添加日志的配置文件，打印日志的本地文件

--- src/main/java/com/sell/demo/LoggerTest.java
+++ src/main/java/com/sell/demo/LoggerTest.java
@@ -1,23 +1,25 @@
 package com.sell.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class LoggerTest {
-
-    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test1(){
 
-        logger.debug("debug...");
-        logger.info("info...");
-        logger.error("error...");
+        String name = "jinersong";
+        int passworld = 123456;
+        log.info("info ....");
+        log.info("name: {}, passworld: {} ",name,passworld);
+        log.debug("debug ...");
+        log.error("error ...");
+
     }
 }
